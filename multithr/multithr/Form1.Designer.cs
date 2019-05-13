4ddc8eb51c0110a4cc12d78df8494d5e97c9f955
Maciej Anglart <maciej.x.anglart@gsk.com>
2019-05-13 13:23:13 +0200

    commit

---
+++ multithr/multithr/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace multithr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(133, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START/ PAUSE";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(22, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "n:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(22, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "n! =";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(62, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "wynik";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(22, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(179, 19);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "czas od początku obliczeń:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(22, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(199, 19);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "czas do zakończenia obliczeń:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(243, 147);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 19);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "[s]";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(225, 180);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 19);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "???";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(26, 106);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(279, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.IndianRed;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(219, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 45);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "STOP";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(49, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(58, 27);
+            this.textBox1.TabIndex = 11;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(207, 147);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(30, 19);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "???";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.Location = new System.Drawing.Point(261, 180);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(26, 19);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "[s]";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 225);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button button1;
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Label label3;
+        public System.Windows.Forms.Label label4;
+        public System.Windows.Forms.Label label5;
+        public System.Windows.Forms.Label label6;
+        public System.Windows.Forms.Label label7;
+        public System.Windows.Forms.ProgressBar progressBar1;
+        public System.Windows.Forms.Button button3;
+        public System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.Label label8;
+        public System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ multithr/multithr/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace multithr
+{
+    public partial class Form1 : Form
+    {
+        bool isPaused = true;
+        bool isEnd = true;
+        bool stop = false;
+        int i = 1;
+        int n;
+
+        public Form1()
+        {
+            var display_thread = new Thread(() =>
+            {
+                InitializeComponent();
+            });
+
+            display_thread.Start();
+            display_thread.Join();
+        }
+
+        public int getNextFactorial(int number, int result)
+        {
+            return result*number;
+        }
+
+        public void timePrediction_thread()
+        {
+            double predictedTime = 0.5 * n;
+            double timeToEnd = predictedTime - i * 0.5;
+
+            while(true)
+            {
+                timeToEnd = predictedTime - i * 0.5;
+                Thread.Sleep(734);
+                this.BeginInvoke((Action)delegate ()
+                {
+                    label7.Text = timeToEnd.ToString();
+                    if (i <= n)
+                    {
+                        progressBar1.Value = (int)(((double)i / (double)n) * 100);
+                    }
+                    else
+                    {
+                        progressBar1.Value = 100;
+                    }
+                });
+
+                if(isEnd == true)
+                {
+                    break;
+                }
+            }
+        }
+
+        public void calculations_thread()
+        {
+            n = int.Parse(textBox1.Text);
+            
+            int result=1;
+            isEnd = false;
+            double time = 0;
+            while (i <= n)
+            {
+                result = getNextFactorial(i, result);
+                Console.Write("\n" + result);
+                Thread.Sleep(500);
+                time += 0.5;
+                if(time%1 == 0)
+                {
+                    this.BeginInvoke((Action)delegate ()
+                    {
+                        label8.Text = time.ToString();
+                    });
+                }
+                
+                i++;
+
+                while (isPaused == true)
+                {
+                    Thread.Sleep(100);
+                }
+
+                if(stop == true)
+                {
+                    break;
+                }
+            }
+
+            if (!stop)
+            {
+                this.BeginInvoke((Action)delegate ()
+                {
+                    label3.Text = result.ToString();
+                });
+            }
+            isEnd = true;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Thread calculations = new Thread(calculations_thread);
+            Thread predicitons = new Thread(timePrediction_thread);
+            isPaused = !isPaused;
+
+            if(isPaused == false && isEnd == true)
+            {
+                calculations.Start();
+            }
+            predicitons.Start();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            stop = true;
+        }
+    }
+}
